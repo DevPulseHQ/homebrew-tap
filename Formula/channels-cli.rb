7c73ddb967397edec5a718d9e4e66c161505e621
John Gregoriadis <jonno.nz@gmail.com>
2024-03-26 22:29:37 +1300

    Update forumla

--- Formula/channels-cli.rb
+++ Formula/channels-cli.rb
@@ -1,12 +1,12 @@
 class ChannelsCli < Formula
-    desc "Capture and relay requests directly to your development environment"
-    homepage "https://github.com/DevPulseHQ/channels-cli"
-    url "https://github.com/DevPulseHQ/channels-cli/archive/refs/tags/v0.0.2.tar.gz"
-    sha256 "8ada0454ad067c03558de06337308305068129b148784c9cd3064b80762901f4"
-  
-    depends_on "rust" => :build
-  
-    def install
-      system "cargo", "install", "--path", "."
-    end
+  desc "Capture and relay requests directly to your development environment"
+  homepage "https://github.com/DevPulseHQ/channels-cli"
+  url "https://github.com/DevPulseHQ/channels-cli/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "8ada0454ad067c03558de06337308305068129b148784c9cd3064b80762901f4"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--path", "."
   end
+end
