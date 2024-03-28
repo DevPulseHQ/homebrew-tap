db27069f47e23b411d0c304d457033863a38c4e6
John Gregoriadis <jonno.nz@gmail.com>
2024-03-28 14:42:39 +1300

    Update for first public release

--- Formula/channels-cli.rb
+++ Formula/channels-cli.rb
@@ -1,12 +1,13 @@
 class ChannelsCli < Formula
   desc "Capture and relay requests directly to your development environment"
   homepage "https://github.com/DevPulseHQ/channels-cli"
-  url "https://github.com/DevPulseHQ/channels-cli/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "8ada0454ad067c03558de06337308305068129b148784c9cd3064b80762901f4"
+  url "https://github.com/DevPulseHQ/channels-cli/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "8490c05c094325ca40996efc8492bf4d3cf4b1570862287a51731a488dc8b130"
 
   depends_on "rust" => :build
 
   def install
     system "cargo", "install", "--path", "."
+    bin.install "target/release/devpulse-channels-cli" => "channels-cli"
   end
 end
